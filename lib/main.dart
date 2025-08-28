274c54ed8642c7da1f678f9e777df4b556732516
mohamed shehata <moshehata7272d@gmail.com>
2025-08-28 11:37:03 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ToDoApp());
+}
+class ToDoApp extends StatelessWidget {
+  const ToDoApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
