4278d0bb4a48429400747ec43fee1c7bc7eb1501
mohamed shehata <moshehata7272d@gmail.com>
2025-08-28 14:56:04 -0700

    Some modifications in task item and body

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,6 +7,11 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+
+        backgroundColor: Colors.deepPurple,
+        child: Icon(Icons.add,color: Colors.white,),
+        onPressed: (){}),
       drawer: Drawer(
         surfaceTintColor: Colors.white,
       ),
@@ -14,15 +19,21 @@ class HomeScreen extends StatelessWidget {
         iconTheme: IconThemeData(
           color: Colors.white
         ),
-        bottom: PreferredSize(preferredSize: Size.fromHeight(30),
+        bottom: PreferredSize(preferredSize: Size.fromHeight(100),
          child: Padding(
            padding: const EdgeInsets.all(8.0),
-           child: Text("TaskNest",style: TextStyle(fontSize: 25,color: Colors.white),),
+           child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+             children: [
+               Text("TaskNest",style: TextStyle(fontSize: 30,color: Colors.white),),
+               Image.asset("images/task.png",height: 75,width: 75,)
+             ],
+           ),
          )),
         elevation: 6,
          shape: RoundedRectangleBorder(
     borderRadius: BorderRadius.vertical(
-      bottom: Radius.circular(50),
+      bottom: Radius.circular(120)
     ),
   ),
         backgroundColor: Colors.deepPurple,
--- lib/widgets/task_item.dart
+++ lib/widgets/task_item.dart
@@ -5,19 +5,56 @@ class TaskItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Checkbox(value: false, onChanged: (value) {}),
-      title: Text("title", style: TextStyle()),
-      subtitle: Text(
-        "title",
-        style: TextStyle(fontSize: 12, color: Colors.grey),
-      ),
-      trailing: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Text("11:55 PM",style: TextStyle(color: Colors.grey,fontSize: 10),),
-          Text(" fri 6 ,oct, 2025 ",style: TextStyle(color: Colors.grey,fontSize: 10),)
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        onTap: (){
+          
+        },
+        child: Container(
+          height: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.2),
+                spreadRadius: 2,
+                blurRadius: 10,
+                offset: Offset(2, 2)
+              )
+            ]
+          ),
+          child: Column(
+            children: [
+              ListTile(
+                leading: Checkbox(value: false, onChanged: (value) {}),
+                title: Text("title", style: TextStyle(fontSize: 20, color: Colors.black,fontWeight: FontWeight.bold)),
+                subtitle: Text(
+                  "content",
+                  style: TextStyle(fontSize: 12, color: Colors.black,),
+                ),
+                
+              ),
+               Padding(
+                 padding: const EdgeInsets.only(right: 8.0),
+                 child: Row(
+                  
+                  mainAxisAlignment: MainAxisAlignment.end,
+                   children: [
+                     Column(
+                                 children: [
+                      
+                      Text("11:55 PM",style: TextStyle(color: Colors.grey,fontSize: 10),),
+                      Text(" fri 6 ,oct, 2025 ",style: TextStyle(color: Colors.grey,fontSize: 10),)
+                                 ],
+                               ),
+                   ],
+                 ),
+               ),
+            ],
+          ),
+        ),
       ),
     );
   }
